d168b6db17a1ee71762471de03cc24b731e90193
adamj <amrachek@outlook.com>
2020-05-21 15:56:43 -0600

    Finish OOP Inheritance exercises

---
+++ lesson_2/lecture_inheritance.rb
@@ -0,0 +1,59 @@
+class Pet
+  def run
+    'running!'
+  end
+
+  def jump
+    'jumping!'
+  end
+end
+
+class Dog < Pet
+  def speak
+    'bark!'
+  end
+
+  def swim
+    'swimming!'
+  end
+
+  def fetch
+    'fetching!'
+  end
+  
+end
+
+class Bulldog < Dog
+  def swim
+    "can't swim!"
+  end
+end
+
+class Cat < Pet
+  def speak
+    'meow!'
+  end
+end
+
+pete = Pet.new
+kitty = Cat.new
+dave = Dog.new
+bud = Bulldog.new
+
+puts pete.run                # => "running!"
+# pete.speak              # => NoMethodError
+
+puts kitty.run               # => "running!"
+puts kitty.speak             # => "meow!"
+# kitty.fetch             # => NoMethodError
+
+puts dave.speak              # => "bark!"
+
+puts bud.run                 # => "running!"
+puts bud.swim                # => "can't swim!"
+
+# What is the method lookup path and how is it important?
+
+# The method lookup path is the order in which Ruby will move up the class heirarchy to look for methods called on object instances.
+# If a method isn't found in the class of an object, Ruby look in the next super-class (or module) up the chain until it finds the method.
+# You can see the method lookup path for a class by calling `.ancestors` on the class.
