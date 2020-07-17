9c8a1c8910634815f9880572dd0d9a49675b03f4
adamj <amrachek@outlook.com>
2020-07-16 19:17:36 -0600

    Add move history tracking.

--- lesson_2/projects/rps/rps.rb
+++ lesson_2/projects/rps/rps.rb
@@ -1,5 +1,15 @@
 class Move
-  attr_reader :value, :beats
+  attr_reader :value, :beats, :player
+
+  @@all = []
+
+  def initialize(player)
+    @player = player
+  end
+
+  def self.all
+    @@all
+  end
 
   def to_s
     @value
@@ -15,53 +25,59 @@ class Move
 end
 
 class Rock < Move
-  def initialize
+  def initialize(player)
+    super
     @value = 'rock'
     @beats = ['scissors', 'lizard']
   end
 end
 
 class Paper < Move
-  def initialize
+  def initialize(player)
+    super
     @value = 'paper'
     @beats = ['rock', 'spock']
   end
 end
 
 class Scissors < Move
-  def initialize
+  def initialize(player)
+    super
     @value = 'scissors'
     @beats = ['paper', 'lizard']
   end
 end
 
 class Lizard < Move
-  def initialize
+  def initialize(player)
+    super
     @value = 'lizard'
     @beats = ['paper', 'spock']
   end
 end
 
 class Spock < Move
-  def initialize
+  def initialize(player)
+    super
     @value = 'spock'
     @beats = ['rock', 'scissors']
   end
 end
 
 class Player
-  attr_accessor :move, :name, :score
+  attr_accessor :move, :name, :score, :moves
 
   MOVES = {
-      'r' => Rock.new,
-      'p' => Paper.new,
-      'sc' => Scissors.new,
-      'l' => Lizard.new,
-      'sp' => Spock.new
+      'r' => 'Rock',
+      'p' => 'Paper',
+      'sc' => 'Scissors',
+      'l' => 'Lizard',
+      'sp' => 'Spock'
   }
 
   def initialize
     set_name
+    @moves = []
   end
 
   def to_s
@@ -88,7 +104,9 @@ class Human < Player
       choice = gets.chomp
       break if MOVES.keys.include?(choice)
     end
-    self.move = MOVES[choice]
+    move_class = Object.const_get(MOVES[choice])
+    self.move = move_class.new(self)
+    self.moves << move
   end
 end
 
@@ -100,7 +118,8 @@ class Computer < Player
   end
 
   def choose
-    self.move = MOVES[MOVES.keys.sample]
+    move_class = Object.const_get(MOVES.values.sample)
+    self.move = move_class.new(self)
   end
 end
 
@@ -147,11 +166,20 @@ class RPSGame
     puts "Thanks for playing Rock, Paper, Scissors! Goodbye!"
   end
 
+  def choose_moves
+    human.choose
+    computer.choose
+  end
+
   def display_moves
     puts
     puts "You chose #{human.move}."
     puts "The computer chose #{computer.move}."
     puts
+  end
+
+  def update_move_history
+    Move.all << [human.move, computer.move]
   end
 
   def winning_player
@@ -178,14 +206,35 @@ class RPSGame
     puts "---------"
   end
 
+  def view_history?
+    choice = nil
+    loop do
+      puts "Would you like to view all player moves from this session? (y/n)"
+      choice = gets.chomp.downcase
+      break if ['y', 'n'].include?(choice)
+      puts "Sorry, that's not a valid choice."
+    end
+    choice == 'y'
+  end
+
+  def display_history
+    Move.all.each_with_index do |moves, i |
+      puts "--------"
+      puts "Round #{i + 1}:"
+      moves.each do |move|
+        puts "#{move.player.name}: #{move.value}"
+      end
+    end
+  end
+
   def play
     display_welcome_message
 
     loop do
       reset_score
       loop do
-        human.choose
-        computer.choose
+        choose_moves
+        update_move_history
         display_moves
         display_winner(winning_player)
         update_score(winning_player)
@@ -193,6 +242,7 @@ class RPSGame
         break if overall_winner?
       end
       display_overall_winner
+      display_history if view_history?
       break unless play_again?
     end
 
