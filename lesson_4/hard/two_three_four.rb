acf119e39427b980c02597d270030754bfb93736
adamj <amrachek@outlook.com>
2020-08-27 08:05:23 -0600

    Finish Lesson 4 Hard exercises.

---
+++ lesson_4/hard/two_three_four.rb
@@ -0,0 +1,68 @@
+module Moveable
+  attr_accessor :speed, :heading
+  attr_writer :fuel_efficiency, :fuel_capacity
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class WheeledVehicle
+  include Moveable
+
+  def initialize(tire_array, km_traveled_per_liter, liters_of_fuel_capacity)
+    @tires = tire_array
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+  end
+
+  def tire_pressure(tire_index)
+    @tires[tire_index]
+  end
+
+  def inflate_tire(tire_index, pressure)
+    @tires[tire_index] = pressure
+  end
+
+  def range
+    @fuel_capacity * @fuel_efficiency
+  end
+end
+
+class Auto < WheeledVehicle
+  def initialize
+    # 4 tires are various tire pressures
+    super([30,30,32,32], 50, 25.0)
+  end
+end
+
+class Motorcycle < WheeledVehicle
+  def initialize
+    # 2 tires are various tire pressures
+    super([20,20], 80, 8.0)
+  end
+end
+
+class Boat
+  include Moveable
+  attr_reader :propeller_count, :hull_count
+
+  def initialize(num_propellers, num_hulls, km_traveled_per_liter, liters_of_fuel_capacity)
+    self.fuel_efficiency = km_traveled_per_liter
+    self.fuel_capacity = liters_of_fuel_capacity
+    # ... code omitted ...
+  end
+
+  def range
+    super + 10
+  end
+end
+
+class Catamaran < Boat
+end
+
+class Motorboat < Boat
+  def initialize(km_traveled_per_liter, liters_of_fuel_capacity)
+    super(1, 1, km_traveled_per_liter, liters_of_fuel_capacity)
+  end
+end
